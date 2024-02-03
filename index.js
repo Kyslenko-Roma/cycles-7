0fbc166f3d5efc2f5c8efd0c0df85f1776da45a4
Kyslenko-Roma <112819019+Kyslenko-Roma@users.noreply.github.com>
2024-02-03 07:54:38 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// let i = 30;
+//
+// while (i > 0) {
+//     console.log(i);
+//     i--;
+// }
+
+// let i = 30
+// do {
+//     console.log(i);
+//     i--;
+// } while (i > 0);
+
+// for (let i = 30; i > 0; i--){
+//     console.log(i);
+// }
